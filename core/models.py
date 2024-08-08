7d610f39ad46f0ea46e7dcfc83885d8f75188b6a
Victor Oluwadare <111367022+Victoran0@users.noreply.github.com>
2024-08-08 03:37:05 +0100

    Comment functionality working fine

--- core/models.py
+++ core/models.py
@@ -14,22 +14,25 @@ class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     id_user = models.IntegerField()
     bio = models.TextField(blank=True)
-    profileimg = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
+    profileimg = models.ImageField(
+        upload_to='profile_images', default='blank-profile-picture.png')
     location = models.CharField(max_length=100, blank=True)
 
-def __str__(self):
-    return self.user.username
+    def __str__(self):
+        return self.user.username
+
 
 class Post(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    user =models.CharField(max_length=100)
+    user = models.CharField(max_length=100)
     image = models.ImageField(upload_to='post_images')
     caption = models.TextField()
     created_at = models.DateTimeField(default=datetime.now)
     no_of_likes = models.IntegerField(default=0)
 
-def __str__(self):
-    return self.user
+    def __str__(self):
+        return self.user
+
 
 class LikePost(models.Model):
     post_id = models.CharField(max_length=500)
@@ -38,6 +41,7 @@ class LikePost(models.Model):
     def __str__(self):
         return self.username
 
+
 class followersCount(models.Model):
     follower = models.CharField(max_length=100)
     user = models.CharField(max_length=100)
@@ -45,10 +49,12 @@ class followersCount(models.Model):
     def __str__(self):
         return self.user
 
-class commentPost(models.Model):
-    post_id = models.CharField(max_length=500)
+
+class CommentPost(models.Model):
+    comment = models.TextField(max_length=1000000)
     username = models.CharField(max_length=100)
-    comment = models.TextField(default="")
+    post = models.ForeignKey(
+        'Post', on_delete=models.CASCADE, related_name='comments')
 
     def __str__(self):
-        return self.username
+        return 'Comment {} by {}'.format(self.comment, self.username)
